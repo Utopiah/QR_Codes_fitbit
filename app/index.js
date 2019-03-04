e4c168cfd2af68d1465b4a0326e0278204b17652
Fabien Benetou <fabien-services@benetou.fr>
2019-03-04 16:47:29 +0100

    init

---
+++ app/index.js
@@ -0,0 +1,26 @@
+/*
+ * Entry point for the watch app
+ */
+console.log('hi from watch')
+
+/*
+container
+
+    <use id="item1" href="#panoramaview-item">
+      <image x="16" y="10" width="32" height="32" href="icons/linked.png" />
+        <g transform="rotate(90)">
+          <text x="40%" y="-10"
+              font-family="System-Regular" fill="black"
+              font-size="30" font-weight="bold"
+              text-anchor="middle">LinkedIn</text>
+        </g>
+       <image x="20%" width="75%" height="100%" href="qrcodes/linked.png" />
+    </use>
+*/
+
+import document from "document";
+
+/*
+let img = document.getElementById("testing-query");
+img.fill = "green";
+*/
---
+++ companion/index.js
@@ -0,0 +1,19 @@
+/*
+ * Entry point for the companion app
+ */
+import document from "document";
+console.log("Companion code started");
+
+/*
+
+let dots = document.getElementById("pagination-dots");
+
+var i;
+for (i = 0; i < 5; i++) {
+  var dotEl = document.createElement("use")
+  dotEl.setAttribute("href", "#pagination-dot")
+  //dotEl.setAttribute("fill", "red")
+  dots.appendChild(dotEl)
+  console.log("added a dot...")
+} 
+*/
